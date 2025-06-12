649de090929527010cfec91caa99f9ff40e00c34
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-13 01:36:47 +0530

    Feat: v1 -> launch your token component

---
+++ components/TokenLaunchpad.tsx
@@ -0,0 +1,30 @@
+export function TokenLaunchpad() {
+  return (
+    <div className="flex flex-col items-center justify-center p-10 max-w-screen gap-5">
+      <h1 className="text-4xl font-bold mb-4">Solana Token Launchpad</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        className="w-2xs py-5 pl-2.5 border border-gray-600 rounded-xl"
+      />
+      <input
+        type="text"
+        placeholder="Symbol"
+        className="w-2xs py-5 pl-2.5 border border-gray-600 rounded-xl"
+      />
+      <input
+        type="text"
+        placeholder="Image URL"
+        className="w-2xs py-5 pl-2.5 border border-gray-600 rounded-xl"
+      />
+      <input
+        type="text"
+        placeholder="Initial Supply"
+        className="w-2xs py-5 pl-2.5 border border-gray-600 rounded-xl"
+      />
+      <button className="p-5 bg-gray-700 rounded-xl text-white text-xl hover:bg-gray-600 transition-colors duration-300 cursor-pointer">
+        Create a token
+      </button>
+    </div>
+  );
+}
