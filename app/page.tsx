8325ac39eea55521529ca3fad6ca9e4f93321289
Saksham Khare <sakshamkhare19@gmail.com>
2025-06-17 00:03:44 +0530

    Fix: token-launchpad component inside wallet context wrap

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div>
       <RootLayout>
         <ConnectWallet />
+        <TokenLaunchpad />
       </RootLayout>
-      <TokenLaunchpad />
     </div>
   );
 }
